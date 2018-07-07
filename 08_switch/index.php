f761784a5b21fa165130ca616703a4cd64f9219c
tomstormland <tomstormland@gmail.com>
2018-07-07 12:06:03 +0300

    x

---
+++ 08_switch/index.php
@@ -0,0 +1,22 @@
+<?php
+
+$color = 'GREEN';
+
+switch ($color) {
+
+case 'RED':
+  echo 'Color red has been choosed.';
+  break;
+
+case 'BLUE':
+  echo 'You took color blue.';
+  break;
+
+case 'GREEN':
+  echo 'Your color is green.';
+  break;
+
+default:
+  echo 'You don\'t like colors.';
+  break;
+}
